909c7cc2466b9da56d62f6d0b34b75a78f3805cf
Aliyev Temur <aliyer.temur95@gmail.com>
2026-07-04 11:54:37 +0500

    cat

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from product.views import index_list
+from product.views import index_list, create_category
 
 urlpatterns=[
-    path('',index_list,name='index')
+    path('',index_list,name='index'),
+    path('cat/create/',create_category,name='category'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from .forms import CategoryForm
 from .models import Product
 # Create your views here.
 
@@ -9,3 +11,14 @@ def index_list(request):
 
     }
     return render(request,'product/index.html',context)
+
+
+def create_category(request):
+    if request.method=='POST':
+        form=CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        form=CategoryForm()
+    return render(request,'product/category_create.html',{'form':form})
